bb8735f3dbe5434a36f0319b3d47ea059958d3d8
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-29 21:41:59 +0200

    added user model + dependencies

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.status(404).json({ message: "hello world" });
