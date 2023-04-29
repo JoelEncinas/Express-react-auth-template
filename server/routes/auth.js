dac71244a0f21c504cbe140d765bd7df082a693e
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-29 22:01:34 +0200

    login route created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,24 +6,48 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-      const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        res.status(200).json({ message: "Username taken" });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const newUser = new User({ username, password: hashedPassword });
-      await newUser.save();
-
-      // res.status(201).redirect("/auth/login");
-      res.status(201).json({ message: "User created" });
-    } catch (err) {
-      res.status(500).json({ error: "Internal Server Error" });
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.status(200).json({ message: "Username taken" });
     }
-  });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    // res.status(201).redirect("/auth/login");
+    res.status(201).json({ message: "User created" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.status(200).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+      res.cookie("token", token);
+      return res.status(200).json({ message: "Log in successful" });
+    } else {
+      return res.status(200).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
